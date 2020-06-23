10807b2d65e840a51661b3aac39c27d626de74a5
Mukti Wibowo <muktiw7@gmail.com>
2020-06-23 14:36:29 +0700

    Added patch and delete routes

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post('/api/v1/tours', (req, res) => {
   tours.push(newTour);
 
   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201)
+    return res.status(201)
         .json({
           app: 'Natours',
           version: '1.0.0',
@@ -68,6 +68,57 @@ app.post('/api/v1/tours', (req, res) => {
           }
         });
   });
+});
+
+// Patch a tour data
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find(el => el.id === parseInt(req.params.id));
+
+  if (!tour) {
+    return res.status(404)
+              .json({
+                app: 'Natours',
+                version: '1.0.0',
+                status: 'not-found',
+                message: 'Data not found.'
+              });
+  };
+
+  // Patch logic here...
+  tour.difficulty = req.body.difficulty;
+
+  return res.status(201)
+            .json({
+              app: 'Natours',
+              version: '1.0.0',
+              status: 'patched',
+              tour: tour
+            });
+});
+
+// Delete a tour data
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find(el => el.id === parseInt(req.params.id));
+
+  if (!tour) {
+    return res.status(404)
+              .json({
+                app: 'Natours',
+                version: '1.0.0',
+                status: 'not-found',
+                message: 'Data not found.'
+              });
+  };
+
+  // Delete logic here...
+
+  return res.status(204)
+            .json({
+              app: 'Natours',
+              version: '1.0.0',
+              status: 'deleted',
+              tour: tour
+            });
 });
 
 /**
