a2025eb3c01f233af8e4bf8da577aa97f1c204cc
Mukti Wibowo <muktiw7@gmail.com>
2020-06-25 21:25:11 +0700

    Added user schema and model

--- app/model.js
+++ app/model.js
@@ -26,7 +26,33 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price!']
   }
-})
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Missing name field']
+  },
+  username: {
+    type: String,
+    required: [true, 'Missing username field'],
+    unique: [true, 'Username has already been taken']
+  },
+  email: {
+    type: String,
+    required: [true, 'Missing email field'],
+    unique: [true, 'Email has been registered'],
+    match: [
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      'Email address is not valid'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'Missing password field'],
+    minlength: [8, 'Password needs to be at least 8 characters long']
+  }
+});
 
 /**
  * MODEL
@@ -34,3 +60,4 @@ const tourSchema = new mongoose.Schema({
  */
 
 exports.Tour = mongoose.model('Tour', tourSchema, 'tours');
+exports.User = mongoose.model('User', userSchema, 'users');
