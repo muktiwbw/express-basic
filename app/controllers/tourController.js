0cd9a68d9fe83d146e4870d5697c0100978c9f4f
Mukti Wibowo <muktiw7@gmail.com>
2020-06-25 21:56:49 +0700

    Added create function based on Atlas database

--- app/controllers/tourController.js
+++ app/controllers/tourController.js
@@ -1,77 +1,39 @@
-const fs = require('fs');
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../../dev-data/data/tours-simple.json`));
+const {Tour} = require('./../model');
 
 exports.getTours = (req, res) => {
-  if (!req.params.id) {
-    return res.status(200)
-              .json({
-                app: 'Natours',
-                version: '1.0.0',
-                status: 'success',
-                result: tours.length,
-                data: {
-                  tours: tours
-                }
-              });
-  } else {
-    const tour = tours.find(el => el.id === parseInt(req.params.id));
+  res.status(200).json({
+    message: 'Okay'
+  });
+};
 
-    return res.status(200)
+exports.createTours = async (req, res) => {
+  try {
+    const tour = await Tour.create(req.body);
+
+    return res.status(201)
               .json({
-                app: 'Natours',
-                version: '1.0.0',
-                status: 'success',
+                status: 'created',
                 data: {
                   tour: tour
                 }
               });
-  }  
-};
-
-exports.createTours = (req, res) => {
-  const newTour = Object.assign({id: tours.length}, req.body);
-  
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    return res.status(201)
-        .json({
-          app: 'Natours',
-          version: '1.0.0',
-          status: 'created',
-          data: {
-            tour: newTour
-          }
-        });
-  });
+  } catch (err) {
+    return res.status(400)
+              .json({
+                status: 'fail',
+                message: err
+              });
+  }
 };
 
 exports.patchTours = (req, res) => {
-  const tour = tours.find(el => el.id === parseInt(req.params.id));
-
-  // Patch logic here...
-  tour.difficulty = req.body.difficulty;
-
-  return res.status(201)
-            .json({
-              app: 'Natours',
-              version: '1.0.0',
-              status: 'patched',
-              tour: tour
-            });
+  res.status(200).json({
+    message: 'Okay'
+  });
 };
 
 exports.deleteTours = (req, res) => {
-  const tour = tours.find(el => el.id === parseInt(req.params.id));
-
-  // Delete logic here...
-
-  return res.status(204)
-            .json({
-              app: 'Natours',
-              version: '1.0.0',
-              status: 'deleted',
-              tour: tour
-            });
+  res.status(200).json({
+    message: 'Okay'
+  });
 };
