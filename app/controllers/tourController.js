a7ced479ebde8940c04ae06311d88ead20f242e1
Mukti Wibowo <muktiw7@gmail.com>
2020-06-25 23:03:47 +0700

    Added read function to tourController

--- app/controllers/tourController.js
+++ app/controllers/tourController.js
@@ -1,9 +1,44 @@
 const {Tour} = require('./../model');
 
-exports.getTours = (req, res) => {
-  res.status(200).json({
-    message: 'Okay'
-  });
+exports.getTours = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      const tours = await Tour.find();
+
+      return res.status(200)
+                .json({
+                  status: 'success',
+                  result: tours.length,
+                  data: {
+                    tours: tours
+                  }
+                });
+    } else {
+      const tour = await Tour.findById(req.params.id);
+
+      if (!tour) {
+        return res.status(404)
+                  .json({
+                    status: 'not-found',
+                    message: 'Data not found'
+                  });
+      }
+
+      return res.status(200)
+                .json({
+                  status: 'success',
+                  data: {
+                    tour: tour
+                  }
+                });
+    }
+  } catch (err) {
+    return res.status(500)
+              .json({
+                status: 'fail',
+                message: err
+              });
+  }
 };
 
 exports.createTours = async (req, res) => {
