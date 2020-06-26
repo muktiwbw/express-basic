67cd30eae8ebf64ff6668d16749b25e0823c46a2
Mukti Wibowo <muktiw7@gmail.com>
2020-06-26 16:12:27 +0700

    Refactor database files

---
+++ app/database/seeder.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const {Tour} = require('./model');
+
+const seed = async () => {
+  const seeds = JSON.parse(fs.readFileSync(`${__dirname}/seeds.json`, {encoding: 'utf-8'}));
+
+  try {
+    /**
+     * Add more model here...
+     */
+    await Tour.create(seeds.tour);
+    // await ModelName.create(seeds.modelname);
+    
+    /**==================================================== */
+
+    console.log('Data successfully seeded!');
+  } catch (error) {
+    console.log(error);
+  }
+  
+  process.exit();
+}
+
+const scoop = async () => {
+  try {
+    /**
+     * Add more model here...
+     */
+    await Tour.deleteMany();
+    // await ModelName.deleteMany();
+
+    /**================================================== */
+
+    console.log('Data successfully scooped!');
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+}
+
+if (process.argv[2] === 'seed') {
+  seed();
+} else if (process.argv[2] === 'scoop') {
+  scoop();
+} else {
+  console.log(`Invalid flag: ${process.argv[2]}`);
+  process.exit();
+}
