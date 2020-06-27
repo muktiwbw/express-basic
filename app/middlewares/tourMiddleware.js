753094b704e876bcbbcacaecb1fb0b638b469b61
Mukti Wibowo <muktiw7@gmail.com>
2020-06-27 19:36:31 +0700

    Added commonly used endpoint

--- app/middlewares/tourMiddleware.js
+++ app/middlewares/tourMiddleware.js
@@ -1,1 +1,35 @@
-const Tour = require('./../database/model');
+/** =================================================================
+ * COMMONLY USED ENDPOINTS
+ *  Basically you assign the query string manually, and then assign
+ *  the function as the most front middleware.
+ * 
+ *  The middleware stack would look like this:
+ * 
+ *    =========================================
+ *    1. Common endpoint middleware
+ *    =========================================
+ *    2. Utility middleware (optional)
+ *    =========================================
+ *    3. Controller
+ *    =========================================
+ * 
+ *  This way, the end user don't need to put long query string
+ *  when they send a request.
+ * ==================================================================
+ * 
+ * 
+ */
+
+exports.topThreeToursByRating = (req, res, next) => {
+  req.query.sort = '-ratingAverage|price';
+  req.query.fields = 'name|duration|difficulty|ratingAverage|price';
+  req.query.limit = 3;
+
+  next();
+};
+
+/** 
+ * 
+ * COMMONLY USED ENDPOINTS
+ * ==================================================================
+*/
--- app/routes/tourRoute.js
+++ app/routes/tourRoute.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 
+const tourMiddleware = require('./../middlewares/tourMiddleware');
+const utilMiddleware = require('./../middlewares/utils');
+
 const router = express.Router();
+
+/** =================================================================
+ * COMMONLY USED ENDPOINTS
+ * ==================================================================
+ * 
+ * 
+ */
+
+router.route('/top-three-tours-by-rating')
+    .get(
+        tourMiddleware.topThreeToursByRating,
+        utilMiddleware.querySeparator,
+        tourController.getTours
+    );
+
+/** 
+ * 
+ * COMMONLY USED ENDPOINTS
+ * ==================================================================
+*/
 
 router.route('/')
     .post(tourController.createTours);
 
 router.route('/:id?')
-    .get(tourController.getTours)
+    .get(utilMiddleware.querySeparator, tourController.getTours)
     .patch(tourController.patchTours)
     .delete(tourController.deleteTours);
 
