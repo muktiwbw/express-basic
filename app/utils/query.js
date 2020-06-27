6a568b61d20fc36ea3b9214ab02c7df6523845bc
Mukti Wibowo <muktiw7@gmail.com>
2020-06-27 17:01:31 +0700

    Added pagination pipe

--- app/utils/query.js
+++ app/utils/query.js
@@ -1,15 +1,30 @@
-exports.findByQueryString = async (model, filterQuery = null, extraQuery = null) => {
+const breakspace = (args) => {
+  return args.split('|').join(' ');
+};
+
+const paginate = async (query, model, page = 1, perpage = 5) => {
+  const skip = (page - 1) * perpage;
+  const docNum = await model.estimatedDocumentCount();
+
+  if (skip >= docNum) throw 'Page exceeds pagination';
+
+  return query.skip(skip).limit(perpage);
+};
+
+exports.displayByQueryString = async (model, filterQuery = null, extraQuery = null) => {
   /**Filtering */
   let query = model.find(filterQuery || {});
 
   if (extraQuery) {
     /**Sorting */
-    query = extraQuery.sort ? query.sort(extraQuery.sort.split('|').join(' ')) : query.sort('-createdAt');
+    query = extraQuery.sort ? query.sort(breakspace(extraQuery.sort)) : query.sort('-createdAt');
     
-    /**Limit */
-
-    /**Pagination */
+    /**Field projection */
+    query = extraQuery.fields ? query.select(breakspace(extraQuery.fields)) : query.select('-__v');
   }
+
+  /**Pagination */
+  query = await paginate(query, model, extraQuery.page, extraQuery.perpage);
 
   return query;
 }
