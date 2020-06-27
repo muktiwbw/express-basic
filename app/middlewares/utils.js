f5215e3e2b4079e882f8c6cfd53a9f14b10c8624
Mukti Wibowo <muktiw7@gmail.com>
2020-06-27 09:47:52 +0700

    Added advance filter identifier

--- app/middlewares/utils.js
+++ app/middlewares/utils.js
@@ -1,22 +1,27 @@
 exports.querySeparator = (req, res, next) => {
   if (Object.keys(req.query).length > 0) {
-    const dirtyQueryList = ['sort', 'page'];
+    const extraQueryList = ['sort', 'page'];
 
-    const cleanQuery = {...req.query};
-    const dirtyQuery = {};
+    const filterQuery = {...req.query};
+    const extraQuery = {};
 
-    dirtyQueryList.forEach((q) => {
-      if (Object.keys(cleanQuery).includes(q)) {
-        delete cleanQuery[q];
+    /**Separation */
+    extraQueryList.forEach((q) => {
+      if (Object.keys(filterQuery).includes(q)) {
+        delete filterQuery[q];
       }
 
       if (Object.keys(req.query).includes(q)) {
-        dirtyQuery[q] = req.query[q];
+        extraQuery[q] = req.query[q];
       }
     });
 
-    req.cleanQuery = cleanQuery || {};
-    req.dirtyQuery = dirtyQuery || {};
+    /**Identifying advance filtering */
+    const advanceFilterList = /\b(gte|gt|lte|lt)\b/g;
+
+    /**Add dollar sign ($) to advance filter variables */
+    req.filterQuery = JSON.parse(JSON.stringify(filterQuery).replace(advanceFilterList, match => `$${match}`)) || {};
+    req.extraQuery = extraQuery || {};
   }
   
   next();
