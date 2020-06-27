fffa7035b1fc2cf5268eb6a33f886be49781d46e
Mukti Wibowo <muktiw7@gmail.com>
2020-06-27 22:15:50 +0700

    Environment class-based refactoring

--- app/config/env.js
+++ app/config/env.js
@@ -51,43 +51,4 @@ class Environment {
   }
 }
 
-// let envFileName, dbUri;
-
-// switch (process.env.NODE_ENV) {
-//   case 'development':
-//     envFileName = '.env';
-//     break;
-    
-//   case 'production':
-//     envFileName = 'prod.env';
-//     break;
-    
-//   default:
-//     envFileName = '.env';
-//     break;
-// }
-
-// dotenv.config({path: `${__dirname}/../../${envFileName}`});
-
-// switch (process.env.DB_DRIVER) {
-//   /**
-//    * Add additional database driver here...
-//    */
-//   case 'atlas':
-//     dbUri = 'mongodb+srv://$DB_USERNAME$:$DB_PASSWORD$@$DB_HOST$/$DB_DATABASE$?retryWrites=true&w=majority'
-//     dbUri = dbUri
-//       .replace('$DB_USERNAME$', process.env.DB_USERNAME)
-//       .replace('$DB_PASSWORD$', process.env.DB_PASSWORD)
-//       .replace('$DB_HOST$', process.env.DB_HOST)
-//       .replace('$DB_DATABASE$', process.env.DB_DATABASE);
-//     break;
-
-//   default:
-//     break;
-// }
-
-// exports.appEnv = process.env.NODE_ENV || 'development';
-// exports.appPort = process.env.APP_PORT || 3000;
-// exports.dbUri = dbUri;
-
 module.exports = Environment;
