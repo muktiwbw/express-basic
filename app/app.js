eac2d48b4d36f4c6bcfc6210b36a229d8bb1a5bd
Mukti Wibowo <muktiw7@gmail.com>
2020-06-27 19:36:04 +0700

    Remove utility middleware - Put them to specific middlewares that actually need them

--- app/app.js
+++ app/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
 
-const utilMiddleware = require('./middlewares/utils');
-
 const logger = require('./utils/logger');
 
 const app = express();
@@ -12,8 +10,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use(logger.badResponse);
-
-app.use(utilMiddleware.querySeparator);
 
 /**
  * ================================================================================
