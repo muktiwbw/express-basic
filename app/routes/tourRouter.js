eb260e49cccdb0c0150c7f299ca1ee3be9a52c80
Mukti Wibowo <muktiw7@gmail.com>
2020-06-28 20:48:35 +0700

    Router class-based refactor - Rename Route to Router - Refactor to class - Added parent class router

---
+++ app/routes/tourRouter.js
@@ -0,0 +1,44 @@
+const Router = require('./../config/router');
+const TourController = require('../controllers/tourController');
+const TourMiddleware = require('../middlewares/tourMiddleware');
+
+class TourRouter extends Router {
+  constructor() {
+    super();    
+    /** =================================================================
+     * COMMONLY USED ENDPOINTS
+     * ==================================================================
+     * 
+     * Little note:
+     *  When parsing a class method, make sure to use bind() to bind the class instance.
+     *  This way, the class will be initialized first (sort of).
+     *  If not, the method will be parsed alone, means that it doesn't know
+     *  the other methods and attributes in its class.
+     *  This resolves the common problem of "this" resulting undefined. 
+     */
+    
+    this.router.route('/top-three-tours-by-rating')
+        .get(
+            TourMiddleware.topThreeToursByRating.bind(TourMiddleware),
+            TourMiddleware.querySeparator.bind(TourMiddleware),
+            TourController.getTours.bind(TourController)
+        );
+    
+    /** 
+     * 
+     * COMMONLY USED ENDPOINTS
+     * ==================================================================
+    */
+    
+    this.router.route('/')
+        .post(TourController.createTours.bind(TourController));
+    
+    this.router.route('/:id?')
+        .get(TourMiddleware.querySeparator.bind(TourMiddleware), TourController.getTours.bind(TourController))
+        .patch(TourController.patchTours.bind(TourController))
+        .delete(TourController.deleteTours.bind(TourController));
+    
+  }
+}
+
+module.exports = new TourRouter().router;
---
+++ app/routes/userRouter.js
@@ -0,0 +1,18 @@
+const Router = require('./../config/router');
+const userController = require('../controllers/userController');
+
+class UserRouter extends Router {
+    constructor() {
+        super();
+
+        this.router.route('/')
+            .post(userController.createUsers);
+        
+        this.router.route('/:id?')
+            .get(userController.getUsers)
+            .patch(userController.patchUsers)
+            .delete(userController.deleteUsers);
+    }
+}
+    
+module.exports = new UserRouter().router;
