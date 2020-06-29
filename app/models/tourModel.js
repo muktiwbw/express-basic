3f3c1de5c63300645b5b302198e4f2c36a4e8a02
Mukti Wibowo <muktiw7@gmail.com>
2020-06-29 17:03:35 +0700

    Added: 1. Slug field that added using model middleware based on name 2. Virtual field that isn't stored in db, but only on display (JSON and object)

--- app/models/tourModel.js
+++ app/models/tourModel.js
@@ -1,4 +1,5 @@
 const Database = require('../config/database');
+const { default: slugify } = require('slugify');
 
 class Tour extends Database{
   constructor() {
@@ -11,6 +12,7 @@ class Tour extends Database{
         unique: [true, 'Conflicting name with existed data'],
         trim: true
       },
+      slug: String,
       duration: {
         type: Number,
         required: [true, 'Missing duration field']
@@ -57,7 +59,32 @@ class Tour extends Database{
       startDates: {
         type: [Date]
       }
+    }, {
+      toJSON: { virtuals: true },
+      toObject: { virtuals: true }
     });
+
+    this.schema.pre('save', function(next) { this.slug = slugify(this.name, { lower: true }); next(); });
+
+    /**
+     * Little note:
+     *  Declaring virtuals with get() needs a callback function.
+     *  In that callback function, you can access the document fields
+     *  by using "this".
+     * 
+     *  This is why we use ES5 function instead of ES6 arrow function.
+     *  ES5 function doesn't bind outer scope's "this" automatically
+     *  in them. You actually needs to use .bind() to do that (refers to routers declaration).
+     *  That's why we need ES5 because we only need the "this" in the scope of that specific function.
+     * 
+     *  ES6's arrow function automatically binds outer scope's "this".
+     *  In fact, it uses the "this" of any scope that declared it.
+     * 
+     *  In short, ES6's arrow function doesn't have private "this", and uses outer's scope instead.
+     *  While ES5's only uses the "this" inside of it, and needs extra function (bind()) to bind 
+     *  the outer scope's "this" to it.
+     */
+    this.schema.virtual('durationWeek').get(function() { return Math.ceil(this.duration / 7) });
 
     this.model = this.mongoose.model('Tour', this.schema, 'tours');
   }
